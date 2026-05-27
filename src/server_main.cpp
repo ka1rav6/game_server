96bee979042dd380a4002f05fd94ba1f751ad670
Kairav <kairavdutta@gmail.com>
2026-05-27 18:57:59 +0530

    added parsing function with execute too

--- src/server_main.cpp
+++ src/server_main.cpp
@@ -15,6 +15,38 @@ typedef websocketpp::server<websocketpp::config::asio> ws_server;
 State game_state;
 std::mutex state_mutex;
 
+void parse_and_execute(const std::string& command) {
+    std::istringstream iss(command);
+    std::string action, target;
+    iss >> action >> target;
+    int player_index = -1;
+    if (target.rfind("player", 0) == 0) {
+        try {
+            player_index = std::stoi(target.substr(6));
+        } catch (...) {
+            std::cerr << "[server] invalid player index in: " << command << "\n";
+            return;
+        }
+    } else {
+        std::cerr << "[server] unknown target: " << target << "\n";
+        return;
+    }
+    if (player_index < 0 || player_index >= (int)game_state.players.size()) {
+        std::cerr << "[server] player index out of range: " << player_index << "\n";
+        return;
+    }
+    if (action == "moveUp") 
+        game_state.players[player_index].moveUp();
+    else if (action == "moveDown")
+        game_state.players[player_index].moveDown();
+    else if (action == "moveLeft")
+        game_state.players[player_index].moveLeft();
+    else if (action == "moveRight")
+        game_state.players[player_index].moveRight();
+    else
+        std::cerr << "[server] unknown action: " << action << "\n";
+}
+
 
 int main() {
     initializePosition(game_state);
@@ -26,25 +58,20 @@ int main() {
     server.set_error_channels(websocketpp::log::elevel::all);
     server.init_asio();
     server.set_reuse_addr(true);
-    server.set_message_handler(
-        [&server](websocketpp::connection_hdl hdl, ws_server::message_ptr msg) {
-            on_message(&server, hdl, msg);
+    server.set_message_handler( [&server](websocketpp::connection_hdl hdl, ws_server::message_ptr msg){ 
+        on_message(&server, hdl, msg);
         }
     );
-
     server.set_open_handler([](websocketpp::connection_hdl hdl) {
         std::cout << "[server] client connected\n";
     });
-
     server.set_close_handler([](websocketpp::connection_hdl hdl) {
         std::cout << "[server] client disconnected\n";
     });
-
     server.listen(9002);
     server.start_accept();
 
     std::cout << "[server] listening on port 9002 with 8 threads\n";
-
     const int NUM_THREADS = 8;
     std::vector<std::thread> threads;
     for (int i = 0; i < NUM_THREADS - 1; i++) {
@@ -52,9 +79,7 @@ int main() {
             server.run();
         });
     }
-
     server.run();
-
     for (auto& t : threads) {
         t.join();
     }
